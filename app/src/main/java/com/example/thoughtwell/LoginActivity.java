4d586e8b4e7c306c9d2a948a1127490a18f41ddf
Jean Perez <jpere905@fiu.edu>
2021-04-06 23:46:28 -0400

    added LoginActivity

---
+++ app/src/main/java/com/example/thoughtwell/LoginActivity.java
@@ -0,0 +1,63 @@
+package com.example.thoughtwell;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LoginActivity extends AppCompatActivity {
+
+    final String TAG = "LOGIN_ACTIVITY";
+    // for signing in the user anonymously
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+        Log.d(TAG, "entered the 'onCreate' function");
+        // initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+
+    }
+
+
+//    private void signInAnonymously() {
+//
+//        Log.d(TAG, "entered the signInAnon func");
+//        // [START signin_anonymously]
+//        mAuth.signInAnonymously()
+//                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            // Sign in success, update UI with the signed-in user's information
+//                            Log.d(TAG, "signInAnonymously:success");
+//                            FirebaseUser user = mAuth.getCurrentUser();
+//
+//                        } else {
+//                            // If sign in fails, display a message to the user.
+//                            Log.w(TAG, "signInAnonymously:failure", task.getException());
+//
+//                        }
+//                    }
+//                });
+//        // [END signin_anonymously]
+//    }
+
+
+}
--- app/src/main/java/com/example/thoughtwell/MainActivity.java
+++ app/src/main/java/com/example/thoughtwell/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.thoughtwell;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,27 +16,41 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
+    public static final String EXTRA_MESSAGE = "com.example.thoughtwell.MESSAGE";
 
-    private Button btnNext;
-    private Button btnDeposit;
+    //private Button btnNext;
+    //private Button btnDeposit;
+    private Button btnLogin;
     private TextView tvSampleThought;
 
     FirebaseDatabase database = FirebaseDatabase.getInstance();
-    DatabaseReference myRef = database.getReference("message2");
+    DatabaseReference myRef = database.getReference().child("test");
 
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "entered onCreate");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // get references to buttons
-        btnNext = findViewById(R.id.btnNext);
-        btnDeposit = findViewById(R.id.btnDeposit);
+        //btnNext = findViewById(R.id.btnNext);
+        // btnDeposit = findViewById(R.id.btnDeposit);
+        btnLogin = findViewById(R.id.btnLogin);
         tvSampleThought = findViewById(R.id.tvSampleThought);
 
-        myRef.setValue("Another test yet again");
+        String words = "dont stand there come in";
+        tvSampleThought.setText(words);
+        myRef.setValue(words);
+
+    }
+
+    public void gotoLogin(View view) {
+        Log.d(TAG, "entered gotoLogin");
+        Intent intent = new Intent(this, LoginActivity.class);
+        Log.d(TAG, "entered gotoLogin2");
+        startActivity(intent);
 
     }
 }
